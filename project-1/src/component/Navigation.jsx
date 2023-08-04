1d6204323cc15a3965fb215cdea7491fe6735673
abhay3366 <tabhaykant2@gmail.com>
2023-08-04 17:37:00 +0530

    add project 1

---
+++ project-1/src/component/Navigation.jsx
@@ -0,0 +1,24 @@
+
+
+const Navigation = () => {
+  return (
+    <>
+      <nav className="container">
+        <div className="logo">
+          <img src="../public/images/brand_logo.png" alt="brand-logo"></img>
+
+        </div>
+        <ul>
+          <li>Menu</li>
+          <li>Location</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+        <button>login</button>
+        
+      </nav>
+    </>
+  )
+}
+
+export default Navigation
